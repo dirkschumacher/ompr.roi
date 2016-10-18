6ea3b478a9b295aa78e91fd2528ccfb5fa9d86d7
Dirk Schumacher <mail@dirk-schumacher.net>
2016-10-18 19:11:55 +0200

    Update package description

--- R/ompr.roi.R
+++ R/ompr.roi.R
@@ -1,7 +1,8 @@
-#' ompr.roi
+#' A Solver for ompr that uses the R Optimization Infrastructure (ROI)
 #'
-#' Solve OMPR models with ROI
-#'
+#' A solver for ompr based on the R Optimization Infrastructure (ROI).
+#' The package makes all solvers in ROI available to solve ompr models. See the
+#' ompr website/package docs for examples on how to use it.
 #' @docType package
 #' @name ompr.roi
 NULL
